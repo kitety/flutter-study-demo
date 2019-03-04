8d2bd53c4207c53f1e06b3b23151f9a9d1e7e071
kitety <aizaizuori@foxmail.com>
2019-03-04 21:51:03 +0800

    text属性

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,36 @@
+// 引入样式
 import 'package:flutter/material.dart';
 
-// void main() => runApp(MyApp()); 和下面等价
-void main(){
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
+  // 覆盖
   @override
-  Widget build(BuildContext context){
+  // 返回组件
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title:'Welcome To Flutter!',
-      home:Scaffold(
-        appBar: AppBar(
-          title:Text('Welcome To Flutter')
+      title: 'Text Widget',
+      // 主体
+      home: Scaffold(
+        // 垂直居中 布局组件
+        body: Center(
+          child: Text(
+            'Hello Widget!I am FER.Hello Widget!I am FER.Hello Widget!I am FER.Hello Widget!I am FER.Hello Widget!I am FER.',
+            // 居中对齐
+            textAlign: TextAlign.center,
+            // 最多一行
+            maxLines: 1,
+            // 字符超出的话是省略号
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontSize: 25.0,
+              color: Color.fromARGB(255, 255, 125, 125),
+              decoration: TextDecoration.underline,
+              decorationStyle:TextDecorationStyle.solid
+            ),
+          ),
         ),
-        body:Center(
-          child: Text('Hello World！'),
-        )
-      )
+      ),
     );
   }
 }
